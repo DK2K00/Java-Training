c3564afa0f78d2143a5c05832bc49581af1c0fef
Danush Kumar <danushkumar27@gmail.com>
2022-05-01 13:28:00 +0530

    Char boolean string

---
+++ intro/char_boolean_string.java
@@ -0,0 +1,28 @@
+package intro;
+
+public class char_boolean_string {
+
+  public static void main(String[] args) {
+    char letter = 'D';
+    char uniCode = '\u0044';
+
+    System.out.println(letter);
+    System.out.println(uniCode);
+
+    boolean isOverTwentyone = true;
+
+    //String is not a primitive datatype but a special class
+    //which is basically a group of chars which is only limited by memory
+    String trademark = "Trademark: ";
+    String copyRight = trademark + " \u00a9 2022";
+    System.out.println(copyRight);
+
+    String number = "10";
+    int nextNumber = 50;
+    number = number + nextNumber;
+    System.out.println(number);
+    double num = 69.420d;
+    number += num;
+    System.out.println(number);
+  }
+}
